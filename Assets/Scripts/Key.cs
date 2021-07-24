8335a185d3d79888e87d8eb1914f4a627521949c
SACHIN <sachinwagde67@gmail.com>
2021-07-24 18:59:33 +0530

    added key animation (going up and fade out)

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,30 +5,50 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     private PlayerController playerController;
+    private Rigidbody2D rb;
+    private Collider2D KeyCollider;
+    [SerializeField] private GameObject key;
+    [SerializeField] private float speed;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float duration;
+
 
     private void Awake()
     {
         playerController = FindObjectOfType<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        KeyCollider = GetComponent<BoxCollider2D>();
     }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("player"))
         {
+            KeyCollider.enabled = false;
             playerController.KeyPickUp();
-            Destroy(this.gameObject);
+            rb.velocity = transform.up * Time.deltaTime * speed;
+            StartCoroutine("FadeOutAnimation");
+            Invoke(nameof(DestroyObject), 1f);
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private IEnumerator FadeOutAnimation()
     {
-        
+        float counter = 0;
+        Color spriteColor = spriteRenderer.material.color;
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, counter/duration);
+            Debug.Log(alpha);
+            spriteRenderer.color = new Color(spriteColor.r, spriteColor.g, spriteColor.b, alpha);
+            yield return null;
+        }       
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyObject()
     {
-        
+        Destroy(this.gameObject);
     }
 }
